2b57d7d8927509be33a45412b8b565f49409e0d0
Ben Papp <benpapp_microsoft@users.noreply.github.com>
2023-04-18 17:43:54 -0400

    make update config and test

--- build-tests/moduleminifier-webpack4-sourcemaps-test/src/test/ValidateSourceMap.test.ts
+++ build-tests/moduleminifier-webpack4-sourcemaps-test/src/test/ValidateSourceMap.test.ts
@@ -10,30 +10,39 @@ describe('ValidateSourceMap', () => {
 
   afterEach(() => {});
 
-  it('includes the sourcefile in the sourcemap', () => {
-    const packageJsonLookup: PackageJsonLookup = new PackageJsonLookup();
-    const packageFolder: string | undefined = packageJsonLookup.tryGetPackageFolderFor(__dirname);
-    if (!packageFolder) {
-      throw new Error('Unable to find package folder');
-    }
-
-    const outDirPath: string = path.join(packageFolder, 'dist-prod');
-    const distFiles: string[] = FileSystem.readFolderItemNames(outDirPath);
-
-    const sourceMapName = distFiles.find((element) => element.endsWith('.js.map'));
-    if (!sourceMapName) {
-      console.error('files in outDir are' + distFiles.toString());
-      throw new Error('Unable to find source map');
-    }
-
-    const sourceMapObject = JsonFile.load(path.join(outDirPath, sourceMapName!));
-    if (!sourceMapObject?.sources) {
-      throw new Error('Unable to find sources in source map');
-    }
-    const sourceFilePath = '../src/index.ts';
-    expect(sourceMapObject.sources).toContain(sourceFilePath);
-
-    const actualSourceContent = FileSystem.readFile(path.join(outDirPath, sourceFilePath));
-    const sourceContent = sourceMapObject.sourcesContent[sourceMapObject.sources.indexOf(sourceFilePath)];
+  it('includes the sourcefile in the sourcemaps', () => {
+    validateSourceMap('src/index.ts', 'dist-dev');
+    validateSourceMap('src/submodule.ts', 'dist-dev');
+    validateSourceMap('src/index.ts', 'dist-prod');
+    validateSourceMap('src/submodule.ts', 'dist-prod');
   });
 });
+
+function validateSourceMap(sourceFileName: string, outFolder: string) {
+  const packageJsonLookup: PackageJsonLookup = new PackageJsonLookup();
+  const packageFolder: string | undefined = packageJsonLookup.tryGetPackageFolderFor(__dirname);
+  if (!packageFolder) {
+    throw new Error('Unable to find package folder');
+  }
+
+  const outDirPath: string = path.join(packageFolder, outFolder);
+  const distFiles: string[] = FileSystem.readFolderItemNames(outDirPath);
+
+  const sourceMapName = distFiles.find((element) => element.endsWith('.js.map'));
+  if (!sourceMapName) {
+    console.error('files in outDir are' + distFiles.toString());
+    throw new Error('Unable to find source map');
+  }
+
+  const sourceMapObject = JsonFile.load(path.join(outDirPath, sourceMapName!));
+  if (!sourceMapObject?.sources) {
+    throw new Error('Unable to find sources in source map');
+  }
+  const sourceFilePath = '../' + sourceFileName;
+  const webpackFilePath = 'webpack:///./' + sourceFileName;
+  expect(sourceMapObject.sources).toContain(webpackFilePath);
+
+  const actualSourceContent = FileSystem.readFile(path.join(outDirPath, sourceFilePath));
+  const sourceContent = sourceMapObject.sourcesContent[sourceMapObject.sources.indexOf(webpackFilePath)];
+  expect(sourceContent).toEqual(actualSourceContent);
+}
--- build-tests/moduleminifier-webpack4-sourcemaps-test/webpack.config.js
+++ build-tests/moduleminifier-webpack4-sourcemaps-test/webpack.config.js
@@ -2,9 +2,8 @@
 
 const path = require('path');
 const webpack = require('webpack');
-
 const { HashedFolderCopyPlugin } = require('@rushstack/hashed-folder-copy-plugin');
-const { ModuleMinifierPlugin, LocalMinifier } = require('@rushstack/webpack4-module-minifier-plugin');
+const { ModuleMinifierPlugin, WorkerPoolMinifier } = require('@rushstack/webpack4-module-minifier-plugin');
 const { SetPublicPathPlugin } = require('@rushstack/set-webpack-public-path-plugin');
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
@@ -17,8 +16,8 @@ function generateConfiguration(mode, outputFolderName) {
     },
     output: {
       path: path.join(__dirname, outputFolderName),
-      filename: '[name]_[contenthash].js',
-      chunkFilename: '[id].[name]_[contenthash].js',
+      filename: '[name]_[hash].js',
+      chunkFilename: '[id].[name]_[hash].js',
       hashSalt: '2'
     },
     module: {
@@ -34,31 +33,43 @@ function generateConfiguration(mode, outputFolderName) {
     optimization: {
       minimizer: [
         new ModuleMinifierPlugin({
-          minifier: new LocalMinifier({
-            terserOptions: { sourceMap: true }
-          }),
-          sourceMap: true
+          minifier: new WorkerPoolMinifier({
+            // Cap the number of worker processes to avoid CPU saturation.
+            maxThreads: 1,
+
+            terserOptions: {
+              compress: {
+                passes: 3
+              },
+              mangle: true,
+              sourceMap: true,
+              output: {
+                comments: false,
+                wrap_func_args: false
+              }
+            }
+          })
         })
       ]
-    }
-    // plugins: [
-    //   new webpack.optimize.ModuleConcatenationPlugin(),
-    //   new HashedFolderCopyPlugin(),
-    //   new BundleAnalyzerPlugin({
-    //     openAnalyzer: false,
-    //     analyzerMode: 'static',
-    //     reportFilename: path.resolve(__dirname, 'temp', 'stats.html'),
-    //     generateStatsFile: true,
-    //     statsFilename: path.resolve(__dirname, 'temp', 'stats.json'),
-    //     logLevel: 'error'
-    //   }),
-    //   new SetPublicPathPlugin({
-    //     scriptName: {
-    //       useAssetName: true
-    //     }
-    //   }),
-    //   new HtmlWebpackPlugin()
-    // ]
+    },
+    plugins: [
+      new webpack.optimize.ModuleConcatenationPlugin(),
+      new HashedFolderCopyPlugin(),
+      new BundleAnalyzerPlugin({
+        openAnalyzer: false,
+        analyzerMode: 'static',
+        reportFilename: path.resolve(__dirname, 'temp', 'stats.html'),
+        generateStatsFile: true,
+        statsFilename: path.resolve(__dirname, 'temp', 'stats.json'),
+        logLevel: 'error'
+      }),
+      new SetPublicPathPlugin({
+        scriptName: {
+          useAssetName: true
+        }
+      }),
+      new HtmlWebpackPlugin()
+    ]
   };
 }
 
